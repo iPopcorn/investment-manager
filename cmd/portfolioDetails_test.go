4a1c5e34bb47d06ca4f0ac0df62864da462ce6b8
TaylorPetrillo <taypetrillo@gmail.com>
2024-03-10 15:19:32 +0000

    test: too many args for portfolio-details

--- cmd/portfolioDetails_test.go
+++ cmd/portfolioDetails_test.go
@@ -9,24 +9,38 @@ import (
 
 func TestPortfolioDetails(t *testing.T) {
 	t.Run("Returns an error when portfolio-details command called with no args", func(t *testing.T) {
-		// capture output in program by using a buffer
-		actual := new(bytes.Buffer)
-
-		// initialize root command for test context
-		testCmd := cmd.NewRootCmd()
-
-		// set output of root command to buffer
-		testCmd.SetOut(actual)
-		testCmd.SetErr(actual)
-
-		// pass args to root command - this is how you call sub commands
-		testCmd.SetArgs([]string{"portfolio-details"})
-
-		// execute command and expect error
-		err := testCmd.Execute()
-
-		if err == nil {
-			t.Fatalf("Expected error but received none")
-		}
+		args := []string{"portfolio-details"}
+		expectError(ExecuteCommand(args), t)
 	})
+
+	t.Run("Returns an error when portfolio-details command called with too many args", func(t *testing.T) {
+		args := []string{"portfolio-details", "default", "crypto"}
+
+		expectError(ExecuteCommand(args), t)
+	})
+}
+
+func ExecuteCommand(args []string) error {
+	// capture output in program by using a buffer
+	output := new(bytes.Buffer)
+
+	// initialize root command for test context
+	testCmd := cmd.NewRootCmd()
+
+	// set output of root command to buffer
+	testCmd.SetOut(output)
+	testCmd.SetErr(output)
+
+	// pass args to root command - this is how you call sub commands
+	testCmd.SetArgs(args)
+
+	// execute command
+	return testCmd.Execute()
+}
+
+func expectError(err error, t *testing.T) {
+	t.Helper()
+	if err == nil {
+		t.Fatalf("Expected error but received none")
+	}
 }
